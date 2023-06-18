917b1e41e6cdb5b2206637eb74598390c9e52bf3
fasteks <fastik-07@mail.ru>
2023-06-18 22:22:45 +0300

    feat: implement digit sequence check

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // function check(str, bracketsConfig) {
-  console.log('str', str)
-  console.log('bracketsConfig', bracketsConfig)
+// function check(str, bracketsConfig) {
+  // console.log('str', str)
+  // console.log('bracketsConfig', bracketsConfig)
   let openBrackets = []
   // if (bracketsConfig.length === 2) {
   // openBrackets = [bracketsConfig[0]]
   // } else {
   openBrackets = bracketsConfig.reduce((acc, rec) => [...acc, rec[0]], [])
   // }
-  console.log('openBrackets', openBrackets)
+  // console.log('openBrackets', openBrackets)
 
   const bracketsPair = bracketsConfig.reduce((acc, rec) => {
     // console.log('rec', rec)
@@ -18,25 +18,30 @@ module.exports = function check(str, bracketsConfig) {
     }
     return acc
   }, {})
-  console.log('bracketsPair', bracketsPair)
+  // console.log('bracketsPair', bracketsPair)
 
   let stack = []
   for (let i = 0; i < str.length; i++) {
-    // console.log(str[i])
-    if (str[i] === '|' && stack.at(-1) === '|') {
+    // console.log('str[i]', str[i])
+    if (str[i] === bracketsPair[str[i]] && stack.at(-1) === str[i]) {
+      // console.log('bracketsPair[str[i]]', bracketsPair[str[i]])
       stack.pop()
     } else if (openBrackets.includes(str[i])) {
       // console.log('push')
       stack.push(str[i])
-    } else if (i === 0) {
-      // console.log('i === 0')
-      return false
-    } else if (str[i] === bracketsPair[stack.at(-1)]) {
+    }
+    else if (str[i] === bracketsPair[stack.at(-1)]) {
       // console.log('pop')
       stack.pop()
     }
+    else {
+      return false
+    }
+    // console.log('stack', stack)
   }
   return stack.length === 0
+  // return stack
 }
 
-// console.log(check('||', [['|', '|']]))
+// console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]))
+// console.log(')()')
