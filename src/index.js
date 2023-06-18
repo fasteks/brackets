f913ca0beaee85aee2be77438eecb69ed6e5c342
fasteks <fastik-07@mail.ru>
2023-06-18 21:11:34 +0300

    feat: implement brackets sequence check including | character

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // function check(str, bracketsConfig) {
+  console.log('str', str)
+  console.log('bracketsConfig', bracketsConfig)
+  let openBrackets = []
+  // if (bracketsConfig.length === 2) {
+  // openBrackets = [bracketsConfig[0]]
+  // } else {
+  openBrackets = bracketsConfig.reduce((acc, rec) => [...acc, rec[0]], [])
+  // }
+  console.log('openBrackets', openBrackets)
+
+  const bracketsPair = bracketsConfig.reduce((acc, rec) => {
+    // console.log('rec', rec)
+    if (openBrackets.includes(rec[0])) {
+      // console.log('rec[index + 1]', rec[1])
+      return { ...acc, [rec[0]]: rec[1] }
+    }
+    return acc
+  }, {})
+  console.log('bracketsPair', bracketsPair)
+
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    // console.log(str[i])
+    if (str[i] === '|' && stack.at(-1) === '|') {
+      stack.pop()
+    } else if (openBrackets.includes(str[i])) {
+      // console.log('push')
+      stack.push(str[i])
+    } else if (i === 0) {
+      // console.log('i === 0')
+      return false
+    } else if (str[i] === bracketsPair[stack.at(-1)]) {
+      // console.log('pop')
+      stack.pop()
+    }
+  }
+  return stack.length === 0
 }
+
+// console.log(check('||', [['|', '|']]))
